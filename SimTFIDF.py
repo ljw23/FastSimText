f315fca9bd79e942a9db98ef50bfd0f14b4ccfd8
ljw23 <596951616@qq.com>
2020-10-26 16:24:53 +0800

    ‘分字相似’

--- SimTFIDF.py
+++ SimTFIDF.py
@@ -8,25 +8,38 @@
 '''
 
 # here put the import lib
-import  sklearn as sk
+import sklearn as sk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from scipy.sparse import csr_matrix
+import pandas as pd
+import os
+import numpy as np
+from copy import deepcopy
+
 
 class SimTFIDF(object):
     def __init__(self, candidate_path, queryset_path=None):
         '''
         read candidate sentences, build tfidf model
         '''
-        self.candidate = [sentence.strip() for sentence in open(candidate_path,'r')]
-        self.corpus = self.candidate
+
+        self.candidate = [
+            sentence.strip() for sentence in open(candidate_path, 'r')
+        ]
+        self.corpus = deepcopy(self.candidate)
         if queryset_path:
-            self.query = [[sentence.strip() for sentence in open(queryset_path,'r')]]
+            self.query = [
+                sentence.strip() for sentence in open(queryset_path, 'r')
+            ]
             self.corpus.extend(self.query)
 
         self.tfidf_build()
 
-    def getSimilaritySearch(self,result_path,candidate=None, query=None):
+    def getSimilaritySearch(self,
+                            result_path='result.xlsx',
+                            candidate=None,
+                            query=None):
         '''
         1. 根据tfidf模型生成向量计算相似度
         2.  根据相似度结果生成最终搜索结果
@@ -37,46 +50,47 @@ class SimTFIDF(object):
             candidate_vec: m2 *n
             return:  m1*m2
             '''
-             return query_vec.dot(candidate_vec.T)
+            return query_vec.dot(candidate_vec.T)
 
         def most_sim(similarity):
-            return similarity.argmax(axis=1)
+            return np.asarray(similarity.argmax(axis=1)).squeeze(), similarity.max(axis=1).toarray().squeeze()
 
         if query:
             query_vec = self.tfidf_model.transform(query)
         elif self.query:
-            query_vec =self.query_vec
+            query = self.query
+            query_vec = self.query_vec
         else:
             raise ValueError("No query")
 
         if candidate:
             candidate_vec = self.tfidf_model.transform(candidate)
         elif self.candidate:
-            candidate_vec =self.candidate_vec
+            candidate = self.candidate
+            candidate_vec = self.candidate_vec
         else:
             raise ValueError("No candidate")
-        
+
         similarity = similarity_calculation(query_vec, candidate_vec)
-        most_sim_index = most_sim(similarity)
+        most_sim_index, max_sim_value = most_sim(similarity)
 
-        for _similarity, _most_sim_index in tqdm(zip(similarity,most_sim_index)):
-            
+        max_similar_sentences = [candidate[index] for index in most_sim_index]
 
+        df = pd.DataFrame(pd.Series(query), columns=['query'])
+        df['similarity'] = pd.Series(max_sim_value)
+        df['similar sentence'] = pd.Series(max_similar_sentences)
 
-        
-        
-        self.similarity_calculation()
-        self.getSimilaritySearch()
-        
+        print(df.head())
+
+        df.to_excel(os.path.join('data', result_path), index=None)
 
     def tfidf_build(self):
-        tfidf_vectorizer = TfidfVectorizer(tokenizer=self._tokenizer,stop_words=None)
+        tfidf_vectorizer = TfidfVectorizer(tokenizer=self._tokenizer,
+                                           stop_words=None)
         self.tfidf_model = tfidf_vectorizer.fit(self.corpus)
         self.candidate_vec = self.tfidf_model.transform(self.candidate)
         if self.query:
             self.query_vec = self.tfidf_model.transform(self.query)
-
-    
 
     def _tokenizer(self, sentence):
         for word in sentence:
@@ -84,5 +98,5 @@ class SimTFIDF(object):
 
 
 if __name__ == '__main__':
-    sim = SimTFIDF(candidate_path='data/零件名.txt')
-    print(sim)
+    sim = SimTFIDF(candidate_path='data/零件名.txt', queryset_path='data/车名.txt')
+    sim.getSimilaritySearch()
