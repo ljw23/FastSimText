3f063cd37d77d118226ea73ab1efd0450394e62b
ljw23 <ljw23@mail.ustc.edu.cn>
2020-11-04 23:54:00 +0800

    fix bug

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ sim = SimTFIDF(candidate_path='data/零件名.txt', queryset_path='data/车名.t
 
 @app.get("/query/{query}")
 def get_sim_query(query: str, q: Optional[str] = None):
-    return {"query": query, "q": q, "result":sim.search_query("摩托车")}
+    return {"query": query, "q": q, "result":sim.search_query(query)}
